8cb56df4dc1095aca966843323bfc6135cc903c0
=farooq <=farooqaziz1993@gmail.com>
2026-05-15 09:08:30 +0530

    modify wsgi.py

--- pharmaProject/wsgi.py
+++ pharmaProject/wsgi.py
@@ -1,7 +1,15 @@
 import os
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmaProject.settings')
+
+# Auto-run migrations on startup.
+# If migrations cannot be applied, the startup will fail and the app will not run.
+try:
+    call_command('migrate', '--noinput')
+except Exception as exc:
+    raise RuntimeError('Automatic startup migration failed') from exc
 
 application = get_wsgi_application()
 app = application
