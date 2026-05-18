1e6defb53af01c0c0a6f4469edf880a402fa3d6c
=farooq <=farooqaziz1993@gmail.com>
2026-05-18 16:24:00 +0530

    added purchase analysis

--- easypharma/urls/purchase.py
+++ easypharma/urls/purchase.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from easypharma.views.purchase import PurchaseEntryView, PurchaseListView, SupplierAutocomplete
+from easypharma.views.purchase import(PurchaseEntryView, PurchaseListView, 
+                                        SupplierAutocomplete,SupplierWisePurchaseReportView,SupplierReportDataView)
 
 urlpatterns = [
     path('entry/', PurchaseEntryView.as_view(), name='purchase_entry'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('list/', PurchaseListView.as_view(), name='purchase_list'),
     path('delete/<int:invoice_id>/', PurchaseListView.as_view(), name='purchase_delete'),
     path('api/suppliers/search/', SupplierAutocomplete.as_view(), name='supplier_search_api'),
+    path('report/supplier-wise/', SupplierWisePurchaseReportView.as_view(), name='supplier_wise_purchase_report'),
+    path('supplier_report_data/<int:supplier_id>/', SupplierReportDataView.as_view(), name='supplier_report_data'),
 ]
